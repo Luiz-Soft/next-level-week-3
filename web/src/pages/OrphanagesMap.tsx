52c68e51c463955da0e01499f3b67394aafab5fc
Luiz França <62324134+Luiz-Soft@users.noreply.github.com>
2020-12-22 11:25:25 -0300

    Revert "web front-end finished"
    This reverts commit e0f71b6fbcbb0a5b9601db9610a6ee169a55bd67.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import mapMarkerImg from '../images/map-marker.svg';
 import {Link} from 'react-router-dom'
 import {FiPlus} from 'react-icons/fi'
-import { MapContainer as MapContainer} from "react-leaflet"
-import { TileLayer} from 'react-leaflet';
+
 import '../styles/pages/orphanages-map.css';
-import 'leaflet/dist/leaflet.css';
+
 
 function OrphanagesMap(){
-    
 return(
-    
     <div id="page-map">
         <aside>
 <img src={mapMarkerImg} alt="Happy" />
@@ -24,9 +21,7 @@ esperando a sua visita</p>
        </footer>
        
         </aside>
-<MapContainer center={[-8.034469447273263, -34.903220659279675]}zoom={15} style={{width:'100%', height:'100%'}}>
-    <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-    </MapContainer>
+
         <div></div>
         <Link to="" className="create-orphanage"> </Link>
         <FiPlus size={32} color="FFF"></FiPlus>
