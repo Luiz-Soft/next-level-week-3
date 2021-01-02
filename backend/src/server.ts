9bdbcee638df81c9ac1209b258686c416eaa6697
LuizSoft <luizc.franca@hotmail.com>
2021-01-02 20:36:34 -0300

    finished backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import 'express-async-errors';
+import path from 'path';
 import './database/connection';
 import routes from './routes'
-
-
+import errorHandler from './erros/handler'; 
+import cors from 'cors';
 const app = express();
 
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
-
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler)
 app.listen(3333)
